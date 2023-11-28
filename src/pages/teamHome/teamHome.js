6500f613c7dcc10d93a39079af33cf5c490c99d9
Woonani <nada3377@naver.com>
2023-11-28 17:07:54 +0900

    feat : 폴더구조 세팅

---
+++ src/pages/teamHome/teamHome.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const teamHome = () => {
+  return <div></div>;
+};
+
+export default teamHome;
---
+++ src/pages/userHome/userHome.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const userHome = () => {
+  return <div></div>;
+};
+
+export default userHome;
