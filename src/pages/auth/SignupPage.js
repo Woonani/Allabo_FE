c5cf1a4ab29c3d7ce64f2d75df2c5aaed9bdecc0
Woonani <nada3377@naver.com>
2023-11-29 18:06:58 +0900

    feat : 회원가입페이지 함수 추가

--- src/pages/auth/SignupPage.js
+++ src/pages/auth/SignupPage.js
@@ -5,11 +5,10 @@ import LogoBoxHorizon from "../../components/common/LogoBoxHorizon";
 import FloatingLabelInput from "../../components/common/FloatingLabelInput";
 import Button from "../../components/common/Button";
 import Link from "../../components/common/Link";
-
-import { useNavigate } from "react-router-dom";
+import useSignupform from "../../hooks/useSignupform";
 
 const SignupPage = () => {
-  const navigate = useNavigate();
+  const { formData, handleInputChange, handleSignup } = useSignupform();
 
   // onBlur, // 커서 인풋 태그 밖으로 꺼냈을 때 발생하는 이벤트
   // ...props
@@ -25,33 +24,43 @@ const SignupPage = () => {
 
         <FloatingLabelInput
           label="이메일"
-          // value={}
-          // onChange={}
+          name="email" // input 태그의 name 속성 값 email
+          type="text"
+          value={formData.email} // state의 이름(키) email
+          onChange={handleInputChange}
         />
         <FloatingLabelInput
           label="비밀번호"
-          // value={}
-          // onChange={}
+          name="password"
+          type="password"
+          value={formData.password}
+          onChange={handleInputChange}
         />
         <FloatingLabelInput
           label="비밀번호 확인"
-          // value={}
-          // onChange={}
+          name="cnfrmPassword"
+          type="password"
+          value={formData.cnfrmPassword}
+          onChange={handleInputChange}
         />
         <FloatingLabelInput
           label="이름"
-          // value={}
-          // onChange={}
+          name="name"
+          type="text"
+          value={formData.name}
+          onChange={handleInputChange}
         />
         <FloatingLabelInput
           label="회사명"
-          // value={}
-          // onChange={}
+          name="companyName"
+          type="text"
+          value={formData.companyName}
+          onChange={handleInputChange}
         />
         <Button
           text="회원가입하기"
           backgroundcolor="rgb(95, 95, 95)"
-          onClick={() => navigate("/signup")}
+          onClick={handleSignup}
         />
         <Link
           text="이미 계정이 있으신가요?"
