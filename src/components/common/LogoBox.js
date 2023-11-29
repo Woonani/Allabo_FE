699a51da08a681965f20b2ef27f21b57d7377ae4
Woonani <nada3377@naver.com>
2023-11-29 13:31:13 +0900

    feat : 메인페이지 완성, 공통컴포넌트 추가(Button,LogoBox,Background)

---
+++ src/components/common/LogoBox.js
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import LogoImg from "../../assets/img/logo/logo002.png";
+
+const StyledLogoBox = styled.div`
+display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 500px
+    height: 100px
+    margin: auto;
+    box-sizing: border-box;
+`;
+
+const LogoBox = () => {
+  return (
+    <StyledLogoBox>
+      <img
+        src={LogoImg}
+        width="200"
+        height="200"
+        alt="logo"
+        title="ALLABO"
+        align="left"
+      ></img>
+      <div
+        style={{
+          fontSize: "50px",
+          fontFamily: "Inter",
+          color: "rgb(90, 90, 90)",
+          // fontFamily: "NanumSquareRound Extra Bold",
+          fontWeight: "bold",
+        }}
+      >
+        ALLABO
+      </div>
+    </StyledLogoBox>
+  );
+};
+
+export default LogoBox;
---
+++ src/components/layout/CenteredContainer.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const CenteredContainer = styled.div`
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 40%;
+  height: 70%;
+  min-width: 400px;
+  min-height: 600px;
+  margin: auto;
+  box-shadow: 2px 3px 10px 1px grey;
+  box-sizing: border-box;
+  border-radius: 10px;
+`;
+
+export default CenteredContainer;
