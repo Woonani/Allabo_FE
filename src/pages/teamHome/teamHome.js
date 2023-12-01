f395cdd392e4871cb4d6ca5b8ffd569fde954de5
Woonani <nada3377@naver.com>
2023-12-01 15:50:44 +0900

    hotfix : 파일명 소문자 수정을 위한 삭제

--- src/pages/teamHome/teamHome.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const TeamHome = () => {
-  return <div></div>;
-};
-
-export default TeamHome;
--- src/pages/userHome/userHome.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const UserHome = () => {
-  return <div>로그인후 나오는 첫 화면</div>;
-};
-
-export default UserHome;
