7e0e2ddf920f45c1e626d0c6429c6107fe2351ba
Woonani <nada3377@naver.com>
2023-12-04 17:24:16 +0900

    feat : userhome페이지 생성, 버튼 이미지 추가

---
+++ src/components/common/SquareButton.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledSquareBtn = styled.button`
+  width: ${(props) => props.$width || "40px"};
+  height: ${(props) => props.$height || "40px"};
+  border: none; /* 기본적인 테두리 제거 등 */
+  //   box-shadow: 0.1px 0.1px 5px 0.2px grey;
+  background-color: transparent;
+  background-image: url(${(props) => props.$imgUrl || null});
+  background-size: cover; /* 이미지가 버튼을 채우도록 크기 조절 */
+  background-position: center; /* 이미지를 가운데로 정렬 */
+  cursor: pointer;
+  opacity: ${(props) => props.opacity || "100%"};
+`;
+
+const SquareButton = ({ ...props }) => {
+  return (
+    <StyledSquareBtn
+      $imgUrl={props.imgUrl}
+      $width={props.width}
+      $height={props.height}
+    >
+      {props.imgUrl ? null : "버튼"}
+    </StyledSquareBtn>
+  );
+};
+
+export default SquareButton;
--- src/pages/userHome/UserHome.js
+++ src/pages/userHome/UserHome.js
@@ -1,7 +1,64 @@
 import React from "react";
+import styled from "styled-components";
+import SquareButton from "../../components/common/SquareButton";
+import AddBtn from "../../assets/img/common/AddBtn.png";
 
-const UserHome = () => {
-  return <div>유저홈입니다.</div>;
+const StyledContainer = styled.div`
+  width: 70vw;
+  height: 50vh;
+  background-color: var(--color-opacity-blue);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+  border-radius: 15px;
+  padding: 30px;
+`;
+
+const StyledOneline = styled.div`
+  display: flex;
+  justify-content: right;
+  width: 70vw;
+`;
+
+const StyledTitle = styled.h1`
+  font-family: NanumSquareRound Bold;
+  font-size: 30px;
+  width: 70vw;
+  line-height: 1.3;
+`;
+
+const StyledSubTitle = styled.h1`
+  font-family: NanumSquareRound Regular;
+  font-size: 20px;
+  line-height: 1.3;
+`;
+
+const UserHome = (userNick) => {
+  return (
+    <div>
+      <StyledTitle>
+        반가워요! {{ userNick } && "USER님"} <br />
+        워크스페이스를 생성하여 협업에 참여해보세요!
+        <br />
+      </StyledTitle>
+      <br />
+      <br />
+      <br />
+      <StyledOneline>
+        <StyledSubTitle>워크스페이스 생성하기</StyledSubTitle>
+        &nbsp;
+        <SquareButton imgUrl={AddBtn} opacity="0%" width="30px" height="30px" />
+      </StyledOneline>
+      <StyledContainer>
+        {{ userNick } && (
+          <StyledSubTitle>
+            "현재 참여중인 워크스페이스가 없습니다."
+          </StyledSubTitle>
+        )}
+      </StyledContainer>
+    </div>
+  );
 };
 
 export default UserHome;
