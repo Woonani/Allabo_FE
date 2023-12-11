1bda3a38ec12dba9510d8c73c0f8bec51558d59f
Woonani <nada3377@yonsei.ac.kr>
2023-12-11 20:04:19 +0900

    feat : 스타일 변경

--- src/components/TeamModal.js
+++ src/components/TeamModal.js
@@ -1,39 +1,37 @@
 import React from "react";
 import styled from "styled-components";
 
-const Background = styled.div``;
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: grey;
+  opacity: 50%;
+`;
 
-const Modal = styled.div``;
+const Modal = styled.div`
+  position: absolute;
+  top: 20%;
+  left: 30%;
+  width: 700px;
+  height: 500px;
+  background-color: white;
+  border-radius: 5px;
+  display: felx;
+`;
 
 const Btn = styled.button``;
 
 const TeamModal = ({ isOpen, closeModal, content }) => {
   return (
     <div style={{ display: isOpen ? "block" : "none" }}>
-      <div
-        style={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          width: "100vw",
-          height: "100vh",
-          backgroundColor: "grey",
-          opacity: "50%",
-        }}
-      ></div>
-      <div
-        style={{
-          position: "absolute",
-          top: "20%",
-          left: "30%",
-          width: "600px",
-          height: "500px",
-          backgroundColor: "wheat",
-        }}
-      >
+      <Background />
+      <Modal>
         <button onClick={closeModal}>close</button>
-        팀모달임 {content}
-      </div>
+        <div>팀모달임 {content}</div>
+      </Modal>
     </div>
   );
 };
