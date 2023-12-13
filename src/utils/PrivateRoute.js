5ba2bda27f95b0de7db15485314fed4e46df4e7b
Woonani <nada3377@yonsei.ac.kr>
2023-12-13 18:07:23 +0900

    feat : PrivateRoute기능 추가
    로그인 상태시에만 접근 가능한 페이지 설정 기능

---
+++ src/utils/PrivateRoute.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { getCookie } from "./Cookie";
+import { Navigate, Outlet } from "react-router-dom";
+
+const PrivateRoute = () => {
+  const token = getCookie("token");
+  return token ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
