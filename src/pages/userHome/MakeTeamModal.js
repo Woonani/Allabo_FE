09db611b48c4cf414f09c36c231646760aea9591
Woonani <nada3377@yonsei.ac.kr>
2023-12-14 17:03:54 +0900

    feat : 팀 모달 생성

--- src/pages/userHome/MakeTeamModal.js
+++ src/pages/userHome/MakeTeamModal.js
@@ -6,22 +6,45 @@ import styled from "styled-components";
 import Button from "../../components/common/Button";
 import Title from "../../components/common/Title";
 import SimpleButton from "../../components/common/SimpleButton";
+import LogoBox from "../../components/common/LogoBox";
+import FloatingLabelInput from "../../components/common/FloatingLabelInput";
+import Text from "../../components/common/Text";
+import DropdownInput from "../../components/common/DropdownInput";
 
 const GridContainerRow = styled.div`
   display: grid;
-  grid-template-rows: 10fr 45fr 35fr 10fr;
+  grid-template-rows: 10fr 45fr 5fr 30fr 10fr;
   width: 100%;
   height: 100%;
-  padding: 10px 20px;
+  padding: 10px 30px;
 `;
 const GridContainerColumn = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 1fr 1.3fr;
   width: 100%;
   height: 100%;
+  padding: 10px;
 `;
-const CenteredDiv = styled.div`
+const ColumnDiv = styled.div`
   display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 20px 20px 20px 40px;
+`;
+const ColumnDiv2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 20px;
+`;
+const ColumnDiv3 = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 20px 20px 20px 40px;
+`;
+const RowDiv = styled.div`
+  display: flex;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
 `;
@@ -37,17 +60,50 @@ const MakeTeamModal = ({ isOpen, closeModal }) => {
             justifyContent={"left"}
           />
           <GridContainerColumn>
-            <div>팀사진</div>
-            <div>팀인풋</div>
+            <ColumnDiv2 style={{ backgroundColor: "lightGrey" }}>
+              <LogoBox />
+            </ColumnDiv2>
+            <ColumnDiv>
+              <Text text={"워크스페이스 명"} />
+              <FloatingLabelInput
+                label="팀의 특징을 담아 작성해주세요."
+                width="100%"
+                height="45px"
+              />
+              <br />
+              <Text text={"워크스페이스 소개"} />
+              <FloatingLabelInput
+                label="팀의 목적을 간단히 설명해 주세요."
+                width="100%"
+                height="80px"
+              />
+              <br />
+              <Text text={"팀에서 사용할 이름"} />
+              <FloatingLabelInput
+                label="미작성 히 사용자 이름으로 표시됩니다."
+                width="100%"
+                height="45px"
+              />
+            </ColumnDiv>
           </GridContainerColumn>
+          <Text text={"팀원초대"} margin="10px" />
+
           <GridContainerColumn>
-            <div>멤버리스트</div>
-            <div>초대인풋</div>
+            <ColumnDiv3>멤버리스트</ColumnDiv3>
+            <ColumnDiv3>
+              <DropdownInput />
+            </ColumnDiv3>
           </GridContainerColumn>
-          <CenteredDiv>
-            <Button margin={"0px"} />
-            {/* <SimpleButton /> */}
-          </CenteredDiv>
+          <RowDiv>
+            <Button text={"팀 생성"} margin={"20px"} width={"200px"} />
+            <Button
+              text={"생성 취소"}
+              margin={"20px"}
+              backgroundcolor="#f35d5dde"
+              width={"200px"}
+              onClick={closeModal}
+            />
+          </RowDiv>
         </GridContainerRow>
       </ModalContainer>
     </div>
