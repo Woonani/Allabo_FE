ec356d74dc808ac4a1d37c47cf5fa59358dea533
Woonani <nada3377@yonsei.ac.kr>
2023-12-14 12:05:45 +0900

    style : 주석 추가

--- src/utils/PrivateRoute.js
+++ src/utils/PrivateRoute.js
@@ -4,6 +4,7 @@ import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRoute = () => {
   const token = getCookie("token");
+  //토큰 검증로직 추가
   return token ? <Outlet /> : <Navigate to="/" />;
 };
 
