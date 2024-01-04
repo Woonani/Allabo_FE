8852cd553dc0537430d5f8e31ad04fb39a0c08ae
Woonani <nada3377@yonsei.ac.kr>
2024-01-04 12:40:12 +0900

    fix : 콘솔 key 버그 해결
    unique key 가 아닌  map 함수들 모두 주석 처리

--- src/components/layout/SideMenu.js
+++ src/components/layout/SideMenu.js
@@ -79,23 +79,21 @@ const SideMenu = ({ isSideOpen }) => {
             {menu.map((item, idx) => {
               item.active = idx == nowPage ? true : false;
               return (
-                <>
-                  <Link to={item.link} className="link-style">
-                    <ProfileContainer
-                      key={idx}
-                      justifyContent="flex-start"
-                      backgroundColor="hsl(49.36deg 21.79% 90.45% / 15%)"
-                      imgSrc={item.imgSrc}
-                      imgAlt={item.text}
-                      imgWidth="25px"
-                      imgHeight="25px"
-                      text={item.text}
-                      frontSpaceWidth="10px"
-                      backSpaceWidth="0px"
-                      active={item.active}
-                    />
-                  </Link>
-                </>
+                <Link key={idx} to={item.link} className="link-style">
+                  <ProfileContainer
+                    // key={item.key}
+                    justifyContent="flex-start"
+                    backgroundColor="hsl(49.36deg 21.79% 90.45% / 15%)"
+                    imgSrc={item.imgSrc}
+                    imgAlt={item.text}
+                    imgWidth="25px"
+                    imgHeight="25px"
+                    text={item.text}
+                    frontSpaceWidth="10px"
+                    backSpaceWidth="0px"
+                    active={item.active}
+                  />
+                </Link>
               );
             })}
           </StyledSideOpened>
