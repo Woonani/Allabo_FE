3353489956c810aae0b4d36f639a32037b7d9b6e
Woonani <nada3377@yonsei.ac.kr>
2024-01-04 12:13:00 +0900

    feat : 기존 useEffect 삭제
    사이드메뉴 open/close 함수 사용으로 각각의 페이지에서 사용하던 useEffect 삭제
    리팩토링하여 한군데에서 사용할 예정

--- src/pages/taskBoard/TaskBoard.js
+++ src/pages/taskBoard/TaskBoard.js
@@ -1,9 +1,6 @@
-import React, { useEffect } from "react";
+import React from "react";
 
-const TaskBoard = ({ handleSideMenu }) => {
-  useEffect(() => {
-    handleSideMenu("close");
-  }, []);
+const TaskBoard = () => {
   return <div>회의록게시판(태스크보드)입니다.</div>;
 };
 
