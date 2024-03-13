9870bc227c6999cdfb7658040bc26a4fb9cbadb8
Woonani <nada3377@yonsei.ac.kr>
2024-03-13 16:08:06 +0900

    feat : formmater 함수 추가 및 변경
    formatDatetime (): 날짜+시간 formmatting
    formatDate(): 날짜 formmatting

--- src/utils/Formatter.js
+++ src/utils/Formatter.js
@@ -1,5 +1,17 @@
 import React from "react";
 
 export const formatDatetime = (originStr) => {
-  return originStr.replace(/T(.{5}).*/, " $1");
+  if (originStr) {
+    return originStr.replace(/T(.{5}).*/, " $1").replaceAll("-", ".");
+  } else {
+    return null;
+  }
+};
+
+export const formatDate = (originStr) => {
+  if (originStr) {
+    return originStr.replace(/T.*/, "").replaceAll("-", ".");
+  } else {
+    return null;
+  }
 };
